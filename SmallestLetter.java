1fc2c623783adc727965d344257292fd93417954
AdarshSingh9540 <131537713+AdarshSingh9540@users.noreply.github.com>
2023-07-23 20:04:55 +0530

    Add files via upload

---
+++ SmallestLetter.java
@@ -0,0 +1,32 @@
+//public class SmallestLetter {
+//
+//
+//        public static void main(String[] args) {
+//
+//        }
+//        // -------->> return the index of smallest non >= target
+//
+//    public char nextGreatestLetter(char[] letters, char target) {
+//
+//
+//        int start =0;
+//        int end = letter.length-1;
+//
+//        while (start<=end){
+//            int mid = start+(end-start)/2;
+//
+//            if(target<letter[mid]){
+//                end =mid-1;
+//            }
+//            else {
+//                start = mid+1;
+//
+//            }
+//        }
+//        return  letters[start%letters.length];
+//
+//    }
+//
+//    }
+//
+//
